a9d94c6558b0444ecd24a0802b349089fe269759
abidinadhem <nadhemlaabidi@hotmail.com>
2020-01-30 14:39:10 -0800

    final

---
+++ index.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+ var test=confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+ if(test==true ){
+     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+ }
+ else {
+     window.close()
+ }
+},1000)
